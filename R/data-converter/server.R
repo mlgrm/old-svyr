6eda86b610ffeb55e8223e5251e9c2a0f21e85d3
O Milgrim <mlgrm@bigend.org>
2015-08-10 15:21:30 +0300

    added text in select_multi column names

--- R/data-converter/server.R
+++ R/data-converter/server.R
@@ -8,9 +8,14 @@ shinyServer(function(input,output){
       #browser()
       s <- load.svy(input$data.set$datapath,input$json.form$datapath)
       s <- as.data.frame(lapply(s,function(c){
-        if(is(c,"AsIs"))class(c) <- "matrix"
-        c
+        if(is(c,"AsIs")){
+          class(c) <- "matrix"
+          colnames(c) <- sapply(attributes(c)$children[[1]]$label,
+                                function(e)if(is.list(e)) e$English else e)
         }
+        browser(expr=is.matrix(c))
+        c
+      }
       ))
       write.xlsx(as.data.frame.data.frame(s),file,showNA=FALSE)
     }
