2c5b189f5750bdb44b12e278fa058bbebebd3a16
O Milgrim <mlgrm@bigend.org>
2015-08-24 16:15:28 +0300

    fixed data converter bug

--- R/data-converter/server.R
+++ R/data-converter/server.R
@@ -6,13 +6,29 @@ shinyServer(function(input,output){
     content=function(file){
       if(is.null(input$data.set) || is.null(input$json.form))return(NULL)
       #browser()
-      wb <- svy2wb(input$data.set$datapath,input$json.form$datapath)
-      write.xlsx(as.data.frame.data.frame(s),file,showNA=FALSE)
+      df <- svy2df(input$data.set$datapath,input$json.form$datapath)
+      write.xlsx(df,file,showNA=FALSE)
     }
   )
 })
 
 svy2df <- function(dat,json,questionsAsHeaders=FALSE){
   s <- load.svy(dat,json)
-  if(questionsAsHeaders)
+  if(questionsAsHeaders){}
+  colnames(s) <- gsub("G[0-9]+\\.","",colnames(s))
+  s <- as.data.frame(lapply(s,function(c){
+    if(is(c,"AsIs")){
+      class(c) <- "matrix"
+      colnames(c) <- attr(c,"choices")
+      #       if(is.data.frame(attributes(c)$children[[1]]$label))
+      #         colnames(c) <- attributes(c)$children[[1]]$label$English else
+      #           colnames(c) <- sapply(attributes(c)$children[[1]]$label,
+      #                                 function(e)if(is.list(e)) e$English else e)
+    }
+    #browser(expr=is.matrix(c))
+    c
+  }
+  ))#,optional=TRUE)
+  #browser()
+  s
 }
