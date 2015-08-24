46cac2dffd7d32d7b34c4baf3c96dc7e8791a446
O Milgrim <mlgrm@bigend.org>
2015-08-24 18:09:14 +0300

    use csv instead of write.xlsx for speed

--- R/data-converter/server.R
+++ R/data-converter/server.R
@@ -2,12 +2,12 @@ library(svyr)
 
 shinyServer(function(input,output){
   output$data <- downloadHandler(
-    filename=function()sub("\\.csv$","\\.xlsx", input$data.set$name),
+    filename=function()sub("\\.csv$","-converted.csv", input$data.set$name),
     content=function(file){
       if(is.null(input$data.set) || is.null(input$json.form))return(NULL)
       #browser()
       df <- svy2df(input$data.set$datapath,input$json.form$datapath)
-      write.xlsx(df,file,showNA=FALSE)
+      write.csv(df,file,na="")
     }
   )
 })
